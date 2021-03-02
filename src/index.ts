e980b715d1ba5f8a5df84d5d349546e9684d878f
Misir Jafarov <misir.ceferov@gmail.com>
2021-03-02 13:26:53 +0400

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "path";
+const app = express();
+const port = 8080; // default port to listen
+
+// Configure Express to use EJS
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// define a route handler for the default home page
+app.get("/", (req, res) => {
+  // render the index template
+  res.render("index");
+});
+
+// start the express server
+app.listen(port, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`server started at http://localhost:${port}`);
+});
